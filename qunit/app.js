ff70856b1584bd6e9ed038e20747449424fbffb0
NeoLi <hbbpbb@gmail.com>
2016-08-24 14:45:04 +0800

    qunit

---
+++ qunit/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by pbli on 8/24/16.
+ */
+
+function add(x, y) {
+    return x + y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
---
+++ qunit/app.test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by pbli on 8/24/16.
+ */
+QUnit.test("test app", function (assert) {
+    assert.equal(add(1, 2), 3);
+    assert.equal(multiply(2, 3), 5);
+});
