0eae379dba7fd1bf7a45eb6e62b0714e162add76
NeoLi <hbbpbb@gmail.com>
2016-08-25 21:56:49 +0800

    karma

--- 5.karma/karma.conf.js
+++ 5.karma/karma.conf.js
@@ -32,8 +32,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
-
+        reporters: ['progress', 'kjhtml'],
 
         // web server port
         port: 9876,
@@ -55,11 +54,11 @@ module.exports = function (config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         // browsers: ['Chrome', 'Firefox', 'Safari', 'PhantomJS'],
-        browsers: ['PhantomJS'],
+        browsers: ['Chrome'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false,
+        singleRun: true,
 
         // Concurrency level
         // how many browser should be started simultaneous
