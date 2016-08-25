05229ab4c4d472f3ab7ee8aa723df358ad6ff423
NeoLi <hbbpbb@gmail.com>
2016-08-25 11:15:29 +0800

    mocha

--- 3.mocha/demo.sh
+++ 3.mocha/demo.sh
@@ -2,5 +2,5 @@
 mocha 3.mocha/demo01/add.test.js
 mocha --recursive
 mocha --reporter spec
-../../node_modules/.bin/mocha --reporter mochawesome
+../../node_modules/.bin/mocha --recursive --reporter mochawesome
 mocha --reporter tap|dot|nyan|landing|list|progress|json|min|doc|markdown
