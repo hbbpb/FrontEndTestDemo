3850da007dc517d070acb202f8c13d2fe4618819
NeoLi <hbbpbb@gmail.com>
2016-08-25 11:25:48 +0800

    mocha

--- 3.mocha/demo.sh
+++ 3.mocha/demo.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-mocha 3.mocha/demo01/add.test.js
+#01
+mocha add.test.js
+
+#02
 mocha --recursive
 mocha --reporter spec
+
+#03
 ../../node_modules/.bin/mocha --recursive --reporter mochawesome
 mocha --reporter tap|dot|nyan|landing|list|progress|json|min|doc|markdown
+
+#04
+../../node_modules/mocha/bin/mocha --compilers js:babel-core/register
