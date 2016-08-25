5d46af49fd4f41b1e9261fe66059e40739ad630a
NeoLi <hbbpbb@gmail.com>
2016-08-25 12:47:09 +0800

    mocha

--- 3.mocha/demo.sh
+++ 3.mocha/demo.sh
@@ -17,3 +17,15 @@ mocha --reporter tap|dot|nyan|landing|list|progress|json|min|doc|markdown
 mocha -t 5000 timeout.test.js
 mocha -t 10000 async.test.js
 mocha promise.test.js
+
+#06
+mocha beforeEach.test.js
+
+#07  only  skip
+mocha --recursive
+
+#08 run in browser
+mocha init
+
+#09
+mocha --recursive -R markdown > spec.md
