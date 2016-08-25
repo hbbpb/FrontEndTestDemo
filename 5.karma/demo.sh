be6e3500e262616c4898b5ed6d88bf7a3c3d8c95
NeoLi <hbbpbb@gmail.com>
2016-08-25 15:05:55 +0800

    karma

--- 5.karma/demo.sh
+++ 5.karma/demo.sh
@@ -8,3 +8,22 @@ npm install karma-jasmine karma-chrome-launcher --save-dev
 
 # Run Karma:
 ../node_modules/karma/bin/karma start
+
+# Config
+karma init
+# Which testing framework do you want to use?
+# jasmine|qunit|mocha|nodeunit
+
+# Do you want to use Require.js ?
+# no
+
+# Do you want to capture any browsers automatically ?
+# Chrome|ChromeCanary|Firefox|Safari|PhantomJS|Opera|IE
+
+# What is the location of your source and test files ?
+
+# Should any of the files included by the previous patterns be excluded ?
+
+# Do you want Karma to watch all the files and run the tests on change ?
+# no
+
