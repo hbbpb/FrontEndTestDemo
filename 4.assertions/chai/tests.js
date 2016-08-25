4f50b20b36c3639c3fec5e4d74854cb965d94f57
NeoLi <hbbpbb@gmail.com>
2016-08-25 13:14:24 +0800

    assertions.

--- 4.assertions/chai/tests.js
+++ 4.assertions/chai/tests.js
@@ -1,12 +1,19 @@
 var expect = chai.expect;
+var assert = chai.assert;
+chai.should();
 
-describe('加法函数的测试', function() {
-    it('1 加 1 应该等于 2', function() {
+
+describe('加法函数的测试', function () {
+    it('1 加 1 应该等于 2', function () {
+        //expect style
         expect(add(1, 1)).to.be.equal(2);
     });
 
-    it('任何数加0等于自身', function() {
-        expect(add(1, 0)).to.be.equal(1);
-        expect(add(0, 0)).to.be.equal(0);
+    it('任何数加0等于自身', function () {
+        //assert style
+        assert.equal(add(1, 0), 1);
+
+        //should style
+        add(0, 0).should.be.equal(0);
     });
 });
