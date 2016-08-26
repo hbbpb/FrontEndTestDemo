73c8bc7ca43618a8c3df7a182cc790b15758e622
NeoLi <hbbpbb@gmail.com>
2016-08-26 13:33:57 +0800

    nightwatch

--- 6.nightwatch/tests/google.test.js
+++ 6.nightwatch/tests/google.test.js
@@ -4,13 +4,16 @@
 module.exports = {
     'Demo test Google': function (browser) {
         browser
+            .pause(5000)
             .url('http://www.google.com.hk')
             .waitForElementVisible('body', 1000)
+            .pause(5000)
             .setValue('input[type=text]', 'nightwatch')
             .waitForElementVisible('input[name=btnK]', 1000)
             .click('input[name=btnK]')
             .pause(1000)
             .assert.containsText('#main', 'Night Watch')
+            .pause(5000)
             .end();
     }
 };
