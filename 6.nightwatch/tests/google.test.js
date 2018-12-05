3af83916e78d1f3b16afe8aefe696770eb36b74c
NeoLi <hbbpbb@gmail.com>
2018-12-05 10:42:42 +0800

    code format.

--- 6.nightwatch/tests/google.test.js
+++ 6.nightwatch/tests/google.test.js
@@ -2,18 +2,18 @@
  * Created by pbli on 8/26/16.
  */
 module.exports = {
-    'Demo test Google': function (browser) {
-        browser
-        // .pause(5000)
-            .url('http://www.google.com.hk')
-            .waitForElementVisible('body', 1000)
-            // .pause(5000)
-            .setValue('input[type=text]', 'nightwatch')
-            .waitForElementVisible('input[name=btnK]', 1000)
-            .click('input[name=btnK]')
-            .pause(1000)
-            .assert.containsText('#main', 'Night Watch')
-        // .pause(5000)
-            .end();
-    }
-};
+  'Demo test Google': function (browser) {
+    browser
+    // .pause(5000)
+      .url('http://www.google.com.hk')
+      .waitForElementVisible('body', 1000)
+      // .pause(5000)
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('input[name=btnK]', 1000)
+      .click('input[name=btnK]')
+      .pause(1000)
+      .assert.containsText('#main', 'Night Watch')
+    // .pause(5000)
+      .end()
+  }
+}
